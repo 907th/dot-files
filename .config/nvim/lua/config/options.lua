75e3743263d44a9d1d79aa8c100aaa8eb7cf4d28
Aleksei Chernenkov <aychernenkov@gmail.com>
2026-07-02 11:40:09 +0500

    Update 02.07.2026 11:40

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,5 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.o.winborder = "rounded" -- Add borders to windows.
+vim.g.lazyvim_inlay_hints = false -- Disable inlay hints by default.
 vim.o.exrc = true -- Enable project-local NeoVim configuration files.
 vim.opt.spelllang = "en,ru" -- Configure spell checking languages.
--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -27,8 +27,11 @@ return {
   {
     "zbirenbaum/copilot.lua",
     dependencies = {
-      "copilotlsp-nvim/copilot-lsp", -- For NES functionality.
+      "copilotlsp-nvim/copilot-lsp", -- Required for Next Edit Suggestion (NES).
     },
+    cond = function()
+      return os.getenv("COPILOT_LUA_ENABLED") == "true" -- Enable Copilot only when ENV var set.
+    end,
     opts = {
       nes = {
         enabled = true,
@@ -47,6 +50,35 @@ return {
         ["neotest-golang"] = {
           warn_test_name_dupes = false,
         },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          border = "rounded", -- Add a border around suggestion popup.
+        },
+        documentation = {
+          window = {
+            border = "rounded", -- Add a border around documentation popup.
+          },
+        },
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true, -- Add a border around LSP popup.
       },
     },
   },
